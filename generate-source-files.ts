baeccc27ffd6fa17f5f07376ebbd8ac56e541ad3
Gregory Jacobs <greg@greg-jacobs.com>
2022-09-15 14:54:24 -0400

    Match typescript files to Nx repo

--- generate-source-files.ts
+++ generate-source-files.ts
@@ -1,34 +1,44 @@
+import dedent from 'dedent';
 import fse from 'fs-extra';
 
 for (let i = 1; i <= 100; i++) {
     // Generate file contents
-    let fileContents = '';
+    let fileContents = ``;
     for (let j = 0; j < 100000; j++) {
-        fileContents += `\
-export function doThing${j}() {
-    console.log('Hi ${i} ${j}4');
-}
-`;
+        fileContents += dedent`
+            export function doThing${j}() {
+                console.log('Hi ${i} ${j}');
+            }
+        ` + '\n';
     }
 
     // Output the source file
-    fse.outputFileSync(`libs/lib-${i}/src/index.ts`, fileContents);
+    fse.outputFileSync(`libs/lib-${i}/src/functions.ts`, fileContents);
+
+    // Output a source file with a change
+    fse.outputFileSync(`libs/lib-${i}/src/index.ts`, dedent`
+        export * from './functions';
+
+        console.log('update #${Date.now()}');
+    `);
+
 
     fse.outputFileSync(`libs/lib-${i}/tsconfig.json`, JSON.stringify({
-      extends: '../../tsconfig.base.json',
-      compilerOptions: {
-        rootDir: './src',
-        outDir: './dist'
-      }
-  }, null, 4));
-  fse.outputFileSync(`libs/lib-${i}/package.json`, JSON.stringify({
-      name: `lib-${i}`,
-      version: '0.0.0',
-      scripts: {
-        build: 'rm -rf dist && tsc'
-      },
-      devDependencies: {
-        typescript: '4.8.3'
-      }
-  }, null, 4));
+        extends: '../../tsconfig.base.json',
+        compilerOptions: {
+          rootDir: './src',
+          outDir: './dist'
+        }
+    }, null, 4));
+    
+    fse.outputFileSync(`libs/lib-${i}/package.json`, JSON.stringify({
+        name: `lib-${i}`,
+        version: '0.0.0',
+        scripts: {
+          build: 'rm -rf dist && tsc'
+        },
+        devDependencies: {
+          typescript: '4.8.3'
+        }
+    }, null, 4));
 }
